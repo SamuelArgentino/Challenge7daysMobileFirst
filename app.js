89621a41118183a8e5bd9736c78d7975941ac7b5
SamuelArgentino <argentinodosantos@gmail.com>
2024-07-17 22:26:54 -0300

    Projeto Final

---
+++ app.js
@@ -0,0 +1,9 @@
+function mostrarClientes() {
+    document.getElementById( 'menuconsultas' ).style.display = 'none';
+    document.getElementById( 'menuclientes' ).style.display = 'block';
+}
+
+function mostrarConsultas() {
+    document.getElementById( 'menuclientes' ).style.display = 'none';
+    document.getElementById( 'menuconsultas' ).style.display = 'block';
+}
